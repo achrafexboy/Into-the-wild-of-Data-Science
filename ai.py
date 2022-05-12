e56a155020618d60bb65af3fbdf218c55710c8d6
AmraniMohammed <realto35@gmail.com>
2022-05-12 17:32:47 +0000

    data reduction
    Data reduction UI and code

--- ai.py
+++ ai.py
@@ -1,204 +1,256 @@
 from unittest import result
 import seaborn as sns
 import numpy as np
+import pandas as pd
 
-#Get the data
+# Get the data
 titanic_data = sns.load_dataset('titanic')
 titanic_data.head()
-df = titanic_data.copy()
+df1 = titanic_data.copy()
 
-#Methode class
+
+# Methode class
+
 class MissingValues:
-  
-  #init function
-  def __init__(self, dataFrame, methods, rate = 0.1):
-    self.rate = rate
-    self.dataFrame = dataFrame
-    self.methods = methods
 
-    # Get the missing numerical columns
-    self.df_numeric = dataFrame.select_dtypes(include=[np.number])
-    self.numeric_cols = self.df_numeric.columns.values
-    self.missing_numeric_columns = []
-    for column in self.df_numeric.columns:
-       if self.df_numeric[column].isnull().mean() > self.rate:
-          self.missing_numeric_columns.append(column)
-    
-    # Get the missing categorical columns
-    self.df_categorical = dataFrame.select_dtypes(exclude=[np.number])
-    self.categorical_cols = self.df_categorical .columns.values
-    self.missing_categorical_columns = []
-    for column in self.df_categorical.columns:
-       if self.df_categorical[column].isnull().mean() > self.rate:
-          self.missing_categorical_columns.append(column)
+    # init function
+    def __init__(self, dataFrame, methods, rate=0.1):
+        self.rate = rate
+        self.dataFrame = dataFrame
+        self.methods = methods
 
-    self.all_missing_columns = self.missing_numeric_columns + self.missing_categorical_columns
-  #End init function
+        # Get the missing numerical columns
+        self.df_numeric = dataFrame.select_dtypes(include=[np.number])
+        self.numeric_cols = self.df_numeric.columns.values
+        self.missing_numeric_columns = []
+        for column in self.df_numeric.columns:
+            if self.df_numeric[column].isnull().mean() > self.rate:
+                self.missing_numeric_columns.append(column)
 
+        # Get the missing categorical columns
+        self.df_categorical = dataFrame.select_dtypes(exclude=[np.number])
+        self.categorical_cols = self.df_categorical .columns.values
+        self.missing_categorical_columns = []
+        for column in self.df_categorical.columns:
+            if self.df_categorical[column].isnull().mean() > self.rate:
+                self.missing_categorical_columns.append(column)
 
-  # Row Imputation methode
-  def impute_nan_row(self):
-    self.df_copy = self.dataFrame.copy()
-    self.df_copy = self.df_copy.dropna(axis=0)
-    return self.df_copy
-  #End row Imputation methode
+        self.all_missing_columns = self.missing_numeric_columns + \
+            self.missing_categorical_columns
+    # End init function
 
-  # Column Imputation methode
-  def impute_nan_column(self):
-    self.df_copy = self.dataFrame.copy()
+    # Row Imputation methode
 
-    self.df_copy = self.df_copy.drop(columns=self.all_missing_columns, axis=1)
-    return self.df_copy
-  #End column Imputation methode
+    def impute_nan_row(self):
+        self.df_copy = self.dataFrame.copy()
+        self.df_copy = self.df_copy.dropna(axis=0)
+        return self.df_copy
+    # End row Imputation methode
 
-  # Mean methode
-  def impute_nan_mean(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_numeric_columns:
-      mean = self.df_copy[column].mean()
-      self.df_copy[column].fillna(mean, inplace=True)
-    return self.df_copy
-  #End Mean methode
+    # Column Imputation methode
+    def impute_nan_column(self):
+        self.df_copy = self.dataFrame.copy()
 
-  # median methode
-  def impute_nan_median(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_numeric_columns:
-      median = self.df_copy[column].median()
-      self.df_copy[column].fillna(median, inplace=True)
-    return self.df_copy
-  #End median methode
+        self.df_copy = self.df_copy.drop(
+            columns=self.all_missing_columns, axis=1)
+        return self.df_copy
+    # End column Imputation methode
 
-  # End of Distribution Imputation methode
-  def impute_nan_eod(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_numeric_columns:
-      eod_value = self.df_copy[column].mean() + 3 * self.df_copy[column].std()
-      self.df_copy[column].fillna(eod_value, inplace=True)
-    return self.df_copy
-  #End "End of Distribution Imputation" methode
+    # Mean methode
+    def impute_nan_mean(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_numeric_columns:
+            mean = self.df_copy[column].mean()
+            self.df_copy[column].fillna(mean, inplace=True)
+        return self.df_copy
+    # End Mean methode
 
-  # Arbitrary Value Imputation methode
-  def impute_nan_arbitrary_value(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_numeric_columns:
-      max = self.df_copy[column].max()
-      arb_val = self.arb_value(max)
-      self.df_copy[column].fillna(arb_val, inplace=True)
-    return self.df_copy
-  #End Arbitrary Value Imputation methode
+    # median methode
+    def impute_nan_median(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_numeric_columns:
+            median = self.df_copy[column].median()
+            self.df_copy[column].fillna(median, inplace=True)
+        return self.df_copy
+    # End median methode
 
-  # mode methode
-  def impute_nan_mode(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_categorical_columns:
-      self.df_copy[column].fillna(self.df_copy[column].mode().iloc[0], inplace=True)
-    return self.df_copy
-  #End mode methode
+    # End of Distribution Imputation methode
+    def impute_nan_eod(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_numeric_columns:
+            eod_value = self.df_copy[column].mean(
+            ) + 3 * self.df_copy[column].std()
+            self.df_copy[column].fillna(eod_value, inplace=True)
+        return self.df_copy
+    # End "End of Distribution Imputation" methode
 
-  # Arbitrary value columns methode
-  def impute_nan_arbitrary_columns(self):
-    self.df_copy = self.dataFrame.copy()
-    for column in self.missing_categorical_columns:
-      self.df_copy[column].fillna("Misssing_value", inplace=True)
-    return self.df_copy
-  #End Arbitrary value columns methode
+    # Arbitrary Value Imputation methode
+    def impute_nan_arbitrary_value(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_numeric_columns:
+            max = self.df_copy[column].max()
+            arb_val = self.arb_value(max)
+            self.df_copy[column].fillna(arb_val, inplace=True)
+        return self.df_copy
+    # End Arbitrary Value Imputation methode
 
+    # mode methode
+    def impute_nan_mode(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_categorical_columns:
+            self.df_copy[column].fillna(
+                self.df_copy[column].mode().iloc[0], inplace=True)
+        return self.df_copy
+    # End mode methode
 
-  def arb_value(self, max):
-    if(max < 10): return 9
-    elif(max < 100): return 99
-    elif(max < 1000): return 999
-    elif(max < 10000): return 9999
-    elif(max < 100000): return 99999
-    elif(max < 1000000): return 999999
-    elif(max < 10000000): return 9999999
+    # Arbitrary value columns methode
+    def impute_nan_arbitrary_columns(self):
+        self.df_copy = self.dataFrame.copy()
+        for column in self.missing_categorical_columns:
+            self.df_copy[column].fillna("Misssing_value", inplace=True)
+        return self.df_copy
+    # End Arbitrary value columns methode
+
+    def arb_value(self, max):
+        if(max < 10):
+            return 9
+        elif(max < 100):
+            return 99
+        elif(max < 1000):
+            return 999
+        elif(max < 10000):
+            return 9999
+        elif(max < 100000):
+            return 99999
+        elif(max < 1000000):
+            return 999999
+        elif(max < 10000000):
+            return 9999999
 
 
 def method_chosing(classTest):
     for method in classTest.methods:
-      print("====method====")
-      print(method)
-      if(method == "row"): classTest.dataFrame = classTest.impute_nan_row()
-      elif(method == "column"): classTest.dataFrame = classTest.impute_nan_column()
-      elif(method == "mean"): classTest.dataFrame = classTest.impute_nan_mean()
-      elif(method == "median"): classTest.dataFrame = classTest.impute_nan_median()
-      elif(method == "eod"): classTest.dataFrame = classTest.impute_nan_eod()
-      elif(method == "arbitrary"): classTest.dataFrame = classTest.impute_nan_arbitrary_value()
-      elif(method == "mode"): classTest.dataFrame = classTest.impute_nan_mode()
-      elif(method == "arbitraryCat"): classTest.dataFrame = classTest.impute_nan_arbitrary_columns()
-    return classTest.dataFrame  
+        print("====method====")
+        print(method)
+        if(method == "row"):
+            classTest.dataFrame = classTest.impute_nan_row()
+        elif(method == "column"):
+            classTest.dataFrame = classTest.impute_nan_column()
+        elif(method == "mean"):
+            classTest.dataFrame = classTest.impute_nan_mean()
+        elif(method == "median"):
+            classTest.dataFrame = classTest.impute_nan_median()
+        elif(method == "eod"):
+            classTest.dataFrame = classTest.impute_nan_eod()
+        elif(method == "arbitrary"):
+            classTest.dataFrame = classTest.impute_nan_arbitrary_value()
+        elif(method == "mode"):
+            classTest.dataFrame = classTest.impute_nan_mode()
+        elif(method == "arbitraryCat"):
+            classTest.dataFrame = classTest.impute_nan_arbitrary_columns()
+    return classTest.dataFrame
 
 
+# print("type: ", type(df.isnull().mean()))
+# print(df.isnull().mean()[0])
+results = df1.isnull().mean().to_dict()
 
-#print("type: ", type(df.isnull().mean()))
-#print(df.isnull().mean()[0])
+# print("type: ", type(resultDict))
 
-results =  df.isnull().mean().to_dict()
+# print("dict: ", resultDict)
 
-#print("type: ", type(resultDict))
-
-#print("dict: ", resultDict)
-
-#for key in resultDict:
+# for key in resultDict:
 #   print(key, '->', resultDict[key])
 
 
-#results = df.isnull().mean()
-#data = ""
+# results = df.isnull().mean()
+# data = ""
 
 
 """
 ************* Data Reduction *************
 """
 
+
 class DataReduction:
-  
-  #init function
-  def __init__(self, dataFrame, method, target, n_components):
-    self.df = dataFrame
-    self.method = method
-    self.target = target
-    self.n_components = n_components
 
-    #self.df_numeric = df.select_dtypes('number')
-  #End init function
+    # init function
+    def __init__(self, dataFrame, method, target, n_components):
+        self.df = dataFrame
+        self.method = method
+        self.target = target
+        self.n_components = n_components
+        self.Y = self.df['alive']
+        self.df.drop(['alive'], axis=1)
+        self.df_numeric = self.df.select_dtypes('number')
+        self.df_numeric.fillna(0, inplace=True)
+        self.df_categ = self.df.select_dtypes('category')
+    # End init function
 
+    # start Row PCA methode
 
-  # Row PCA methode
-  def pca(self):
-    from sklearn.decomposition import PCA
-    pca = PCA(n_components=self.n_components)
-    df = df.dropna(axis=0)
-    X = df.select_dtypes('number')
-    Xtr=pca.fit_transform(X)
+    def pca(self):
+        from sklearn.decomposition import PCA
+        pca = PCA(n_components=self.n_components)
 
-    # creating a list of column names
-    column_values = [ f'PC{(i+1)}' for i in range(Xtr.shape[1])]
-      
-    # creating the dataframe
-    self.df1 = pd.DataFrame(data = Xtr,
-                      columns = column_values)
+        print("mocchkila")
+        print(self.df_numeric.isnull().mean())
 
-    print("alive1")
-    self.df1[self.target] = self.df[self.target]
-    print("alive2")
+        Xtr = pca.fit_transform(self.df_numeric)
 
-    return self.df1
-  #End row PCA methode
+        # creating a list of column names
+        column_values = [f'PC{(i+1)}' for i in range(Xtr.shape[1])]
 
+        self.df1 = pd.DataFrame(data=Xtr, columns=column_values)
 
+        self.df1 = pd.concat([self.df1, self.df_categ, self.Y], axis=1)
+
+        # creating the dataframe
+        # self.df1 = pd.DataFrame(data = Xtr,
+        # columns = column_values)
+
+        print("alive1")
+        # self.df1[self.target] = self.df[self.target]
+        print("alive2")
+
+        return self.df1
+    # End row PCA methode
+
+    # start factor analysis methode
+    def fa(self):
+        from sklearn.decomposition import FactorAnalysis
+        transformer = FactorAnalysis(n_components=7, random_state=0)
+
+        X_transformed = transformer.fit_transform(self.df_numeric)
+
+        # creating a list of column names
+        column_values = [
+            f'Factor{(i+1)}' for i in range(X_transformed.shape[1])]
+
+        self.df1 = pd.DataFrame(data=X_transformed, columns=column_values)
+
+        self.df1 = pd.concat([self.df1, self.df_categ, self.Y], axis=1)
+
+        print("alive1")
+        self.df1[self.target] = self.df[self.target]
+        print("alive2")
+
+        return self.df1
+    # End factor analysis methode
 
 
 def method_chosing_data_red(classTest):
     for method in classTest.method:
-      print("====method====")
-      print(method)
-      if(method == "pca"): classTest.df = classTest.pca()
-    return classTest.df1  
+        print("====method====")
+        print(method)
+        if(method == "pca"):
+            classTest.df = classTest.pca()
+        if(method == "fa"):
+            classTest.df = classTest.fa()
+    return classTest.df1
 
 
-#Using exemple :
+# Using exemple :
 
 """
 classTest = DataReduction(df, ["pca"], "alive", n_components)
--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from json import load
 from pickle import TRUE
 import sqlite3
 from flask import render_template, Flask, redirect, url_for, request, make_response, flash, abort
-from ai import results, MissingValues, method_chosing, df, DataReduction, method_chosing_data_red
+from ai import results, MissingValues, method_chosing, DataReduction, method_chosing_data_red, np
+from ai import df1 as dataFrame
 import pandas as pd
 
 app = Flask(__name__)
@@ -12,36 +13,42 @@ app.config['SECRET_KEY'] = '998273675bfbebc4d8be595e'
 
 new_df = pd.DataFrame()  # create a new dataframe
 
+df = dataFrame
+
+
 @app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     rep = make_response(render_template('login.html'))
-    rep.set_cookie('answer','42')
+    rep.set_cookie('answer', '42')
     return rep
+
 
 @app.route('/login', methods=["POST"])
 def succ():
     email = request.form.get("email")
     password = request.form.get("password")
     with sqlite3.connect("database.db") as db:
-      cur = db.cursor()
-      cur.execute("Select * from users where email= ? AND password = ?",(email, password))
-      rows = cur.fetchall()
+        cur = db.cursor()
+        cur.execute(
+            "Select * from users where email= ? AND password = ?", (email, password))
+        rows = cur.fetchall()
     if len(rows) == 0:
         return redirect(url_for('login'))
-    return redirect(url_for('annexe',session = True))
-
+    return redirect(url_for('annexe', session=True))
 
 
 @app.route('/register')
 def register():
     rep = make_response(render_template('Registration.html'))
-    rep.set_cookie('answer','42')
+    rep.set_cookie('answer', '42')
     return rep
+
 
 """
 @app.route('/register', methods=["POST","GET"])
@@ -50,48 +57,53 @@ def registration():
     password = request.form.get("password")
     with sqlite3.connect("database.db") as db:
       cur = db.cursor()
-      cur.execute("INSERT INTO users(email,password) VALUES(?,?)",(email,password))
+      cur.execute("INSERT INTO users(email,password) VALUES(?,?)",
+                  (email,password))
       db.commit()
     return redirect(url_for('login'))
 """
 
 
-
-@app.route('/home', methods=["POST","GET"])
-def annexe(session = True):
-    if session :
+@app.route('/home', methods=["POST", "GET"])
+def annexe(session=True):
+    if session:
         with sqlite3.connect("database.db") as db:
             db.row_factory = sqlite3.Row
             cur = db.cursor()
             c = cur.execute("select * from Track")
             rows = [dict(row) for row in c.fetchall()]
             db.commit()
-        return render_template('home.html', Rows = rows)
-    else : return redirect(url_for('login'))
+        return render_template('home.html', Rows=rows)
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/missingValues', methods=["POST", "GET"])
 def missingValues():
+    global df
     if request.method == 'POST':
         if(request.form.get("rate")):
-            rate = float(request.form.get("rate"))   
-            method = request.form.get("method") #row or column delete
-            numericalMethod = request.form.get("numericalMethod") # mean median eod or arbitrary for numerical data
-            categoricalMethod = request.form.get("categoricalMethod") # mode or arbitrary for categorical data
+            rate = float(request.form.get("rate"))
+            method = request.form.get("method")  # row or column delete
+            # mean median eod or arbitrary for numerical data
+            numericalMethod = request.form.get("numericalMethod")
+            # mode or arbitrary for categorical data
+            categoricalMethod = request.form.get("categoricalMethod")
             print(numericalMethod)
             print(categoricalMethod)
             print("method: ", method)
-            #print("rate: ", rate)
+            # print("rate: ", rate)
 
-            #Infos!!!!!!!!!!!!!!!!!
+            # Infos!!!!!!!!!!!!!!!!!
             "if the user doesn't choose a method for numerical data, the method will be arbitrary"
-            #End of Infos
+            # End of Infos
             if(numericalMethod == None or categoricalMethod == None):
                 flash("Chose a method for numirical and categorical data")
             else:
                 methods = []
-                #Infos!!!!!!!!!!!!!!!!!
+                # Infos!!!!!!!!!!!!!!!!!
                 "if the user chose to delete rows or columns we will ignore the other methods"
-                #End of Infos
+                # End of Infos
                 if(method != None):
                     methods = [method]
                     classTest = MissingValues(df, methods, rate)
@@ -105,39 +117,49 @@ def missingValues():
                     new_df = method_chosing(classTest)
                     print('Numerical: ', classTest.missing_numeric_columns)
                     print('Categorical: ', classTest.missing_categorical_columns)
-            return render_template('missingValues.html', results = df, finalResult = new_df.isnull().mean().to_dict(), new_df = new_df)
-            
+            return render_template('missingValues.html', results=df, finalResult=new_df.isnull().mean().to_dict(), new_df=new_df)
+
             """if(new_df.empty):
-                #flash("No missing values")
+                # flash("No missing values")
                 return render_template('missingValues.html', results = df)
             else:
                 return render_template('missingValues.html', results = df, finalResult = new_df.isnull().mean().to_dict(), new_df = new_df)"""
         else:
             flash("Enter a rate please")
-            #print("error")   
-    return render_template('missingValues.html', results = df)
+            # print("error")
+    return render_template('missingValues.html', results=df)
 
 
 """****Data Reduction****"""
+
+
 @app.route('/dataReduction', methods=["POST", "GET"])
 def dataReduction():
+    global df
+    print(df.isnull().mean().to_dict())
+    print(df.isin([np.inf, -np.inf]).mean().to_dict())
+
     if request.method == 'POST':
         if(request.form.get("n_components") and request.form.get("dataRedMethode")):
-            n_components = float(request.form.get("rate"))   
-            method = [request.form.get("dataRedMethode")] #row or column delete
-            print(n_components)
-            print("method: ", method)
 
-            classTest = DataReduction(df, ["pca"], "alive", n_components)
+            # row or column delete
+            method = [request.form.get("dataRedMethode")]
+            print("n_components: ", request.form.get("n_components"))
+            n_components = int(request.form.get("n_components"))
+            print(n_components)
+            print("method: ", method[0])
+
+            classTest = DataReduction(df, [method[0]], "alive", n_components)
             new_df = method_chosing_data_red(classTest)
-            return render_template('dataReduction.html', results = df, finalResult = new_df)
+            return render_template('dataReduction.html', results=df, data=df.to_html(classes='data'), titlesResult=df.columns.values, finalResult=[new_df.to_html(classes='data')], titles=new_df.columns.values)
 
         else:
             flash("3mar kolch")
-            #print("error")   
-    return render_template('dataReduction.html', results = df)
+            # print("error")
+    return render_template('dataReduction.html', results=df)
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     app.run(debug=True)
 
 
